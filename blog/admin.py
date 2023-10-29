606cc3eddc3c87cb6e60e22618f9e11029348854
Misha <misha.dorozhkin@mail.ru>
2023-10-29 19:39:26 +0300

    Модифицировал вывод и обработку запросов, добавив следующую логику на уровне контроллеров:
    - при открытии отдельной статьи увеличивается счетчик просмотров;
    - выводятся в список статей только те, которые имеют положительный признак публикации;
    - при создании динамически формируется slug name для заголовка;
    - после успешного редактирования записи пользователя перенаправляет на просмотр этой статьи.

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ from blog.models import Blog
 
 @admin.register(Blog)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('Heading', 'slug', 'Content', 'image', 'date_of_creation', 'is_active', 'view_count',)
+    list_display = ('Heading', 'Content', 'image', 'is_active', 'view_count', 'slug')
     list_filter = ('Heading',)
     search_fields = ('id', 'Heading', 'Content', 'date_of_creation',)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
 from blog.models import Blog
 
 
@@ -9,19 +11,50 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy('blog:list')
 
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.Heading)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+
 class BlogListView(ListView):
     model = Blog
     template_name = 'blog/blog_list.html'
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_active=True)
+        return queryset
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.blog = super().get_object(queryset)
+        self.blog.view_count += 1
+        self.blog.save()
+        return self.blog
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('Heading', 'slug', 'Content', 'image', 'date_of_creation', 'is_active', 'view_count',)
-    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.Heading)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
+
 
 
 class BlogDeleteView(DeleteView):
